c5dcc4f2447cf6204693803dd8f708fe163c10b0
bdleppke <bdleppke@hotmail.com>
2024-05-07 17:09:03 -0400

    wworkig

--- main.cpp
+++ main.cpp
@@ -1,3 +1,43 @@
+
+/*
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <cpprest/http_listener.h>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <thread>
+#include <chrono>
+#include <ctime>
+
+// cpprest provides macros for all streams but std::clog in basic_types.h
+#ifdef _UTF16_STRINGS
+// On Windows, all strings are wide
+#define uclog std::wclog
+#else
+// On POSIX platforms, all strings are narrow
+#define uclog std::clog
+#endif // endif _UTF16_STRINGS
+
+using namespace std;
+using namespace web::http::experimental::listener;
+using namespace web::http;
+using namespace web;
+
+void respond(const http_request& request, const status_code& status, const json::value& response) {
+	json::value resp;
+	resp[U("status")] = json::value::number(status);
+	resp[U("response")] = response;
+
+	// Pack in the current time for debugging purposes.
+	time_t now = time(0);
+	utility::stringstream_t ss;
+	ss << put_time(localtime(&now), L"%Y-%m-%dT%H:%S:%MZ");
+	resp[U("server_time")] = json::value::string(ss.str());
+
+	request.reply(status, resp);
+}
+*/
 #include <iostream>		// Include all needed libraries here
 #include <pigpio.h>
 
@@ -5,8 +45,8 @@ using namespace std;		// No need to keep using “std”
 
 #include "ctre/phoenix6/TalonFX.hpp"
 #include "UpLiftBase.hpp"
-#include "Joystick.hpp"
-#include "httplib.h"
+//#include "Joystick.hpp"
+
 
 using namespace ctre::phoenix6;
 
@@ -32,7 +72,7 @@ private:
 
 
     /* joystick */
-    Joystick joy{0};
+ //   Joystick joy{0};
   //  float motorspeed;
     bool buttonpressed = false;
     double maxlift = -877;
@@ -134,7 +174,7 @@ void UpLift::UpLiftInit()
 void UpLift::UpLiftPeriodic()
 {
     /* periodically check that the joystick is still good */
-    joy.Periodic();
+  //  joy.Periodic();
 }
 
 /**
@@ -220,10 +260,10 @@ void UpLift::DisabledPeriodic()
 }
 
 /* ------ main function ------ */
+
 int main()
 {
-
-    gpioInitialise();
+     gpioInitialise();
     gpioSetMode(22, PI_INPUT);
     gpioSetMode(23, PI_INPUT);
     gpioSetPullUpDown(22, PI_PUD_UP);
@@ -234,15 +274,44 @@ int main()
     gpioSetMode(6, PI_INPUT);
     gpioSetPullUpDown(5, PI_PUD_UP);
     gpioSetPullUpDown(6, PI_PUD_UP);
+
     /* create and run uplift */
     UpLift uplift{};
+    return uplift.Run();
     // uplift.SetLoopTime(20_ms); // optionally change loop time for periodic calls
+	// Synchronously bind the listener to all nics.
+	/*
+  uclog << U("Starting listener.") << endl;
+	http_listener listener(U("http://localhost/json"));
+	listener.open().wait();
 
-    uplift.Run();
-    httplib::Server svr;
-    svr.Get("/go", [](const httplib::Request &, httplib::Response &res)
-            { res.set_content("Hello World!", "text/plain"); });
+	// Handle incoming requests.
+	uclog << U("Setting up JSON listener.") << endl;
+	listener.support(methods::GET, [] (http_request req) {
+		auto http_get_vars = uri::split_query(req.request_uri().query());
 
-    svr.listen("0.0.0.0", 8080);
-    return true;
+		auto found_name = http_get_vars.find(U("request"));
+
+		if (found_name == end(http_get_vars)) {
+			auto err = U("Request received with get var \"request\" omitted from query.");
+			uclog << err << endl;
+			respond(req, status_codes::BadRequest, json::value::string(err));
+			return;
+		}
+
+		auto request_name = found_name->second;
+		uclog << U("Received request: ") << request_name << endl;
+		respond(req, status_codes::OK, json::value::string(U("Request received for: ") + request_name));
+	});
+
+	// Wait while the listener does the heavy lifting.
+	// TODO: Provide a way to safely terminate this loop.
+	uclog << U("Waiting for incoming connection...") << endl;
+uplift.Run();
+
+	// Nothing left to do but commit suicide.
+	uclog << U("Terminating JSON listener.") << endl;
+	listener.close();
+	return 0;
+  */
 }
